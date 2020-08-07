9a2bcdcf488fd3deb4b88d310842a8639884f668
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-07 03:01:03 +0300

    01. example usage of hook "useState"

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 
-export default class App extends Component {
-    state = {
-        todos: [
-            { id: 1, title: 'First todo', completed: false },
-            { id: 2, title: 'Second todo', completed: true },
-        ],
+export default function App() {
+    const [todos, setTodos] = useState([
+        { id: 1, title: 'First todo', completed: false },
+        { id: 2, title: 'Second todo', completed: true },
+    ]);
+    const [todoTitle, setTodoTitle] = useState('');
+
+    const addTodo = event => {
+        if (event.key === 'Enter') {
+            setTodos([
+                ...todos,
+                {
+                    id: Date.now(),
+                    title: todoTitle,
+                    completed: false,
+                },
+            ]);
+            setTodoTitle('');
+        }
     };
 
-    render() {
-        return (
-            <div className="container">
-                <h1>Todo app</h1>
+    return (
+        <div className="container">
+            <h1>Todo app</h1>
 
-                <div className="input-field">
-                    <input type="text" />
-                    <label>Todo name</label>
-                </div>
-
-                <TodoList todos={this.state.todos} />
+            <div className="input-field">
+                <input
+                    type="text"
+                    value={todoTitle}
+                    onChange={event => setTodoTitle(event.target.value)}
+                    onKeyPress={addTodo}
+                />
+                <label>Todo name</label>
             </div>
-        );
-    }
+
+            <TodoList todos={todos} />
+        </div>
+    );
 }
--- src/app/components/TodoItem.jsx
+++ src/app/components/TodoItem.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TodoItem({ title, id, completed }) {
+    const [checked, setChecked] = useState(completed);
+
+    const classes = ['todo'];
+
+    if (checked) {
+        classes.push('completed');
+    }
+
     return (
         <li className="todo">
             <label>
-                <input type="checkbox" defaultChecked={false} />
+                <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
+
                 <span>{title}</span>
 
                 <i className="material-icons red-text">delete</i>
